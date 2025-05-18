6bd9199818cde5aecfc09187a8ee4e8286cd9ff1
mic-rwk <michalrywak@gmail.com>
2025-05-18 16:35:57 +0200

    Add models to website, Reservation not working

--- djangodb/website/admin.py
+++ djangodb/website/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-#from .models import Student
+from .models import *
 
 # Register your models here.
-#admin.site.register(Student)
+admin.site.register(Student)
+admin.site.register(Building)
+admin.site.register(Room)
+admin.site.register(Lecturer)
+admin.site.register(Class)
+admin.site.register(Reservation)
+admin.site.register(ReservationStatus)
+admin.site.register(Subject)
