320c60654efa803991cf33bb0da5d3fa47681fb6
mic-rwk <michalrywak@gmail.com>
2025-05-18 15:07:28 +0200

    Import tables to models.py

--- djangodb/djangodb/settings.py
+++ djangodb/djangodb/settings.py
@@ -77,12 +77,7 @@ WSGI_APPLICATION = 'djangodb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-
-    'zajecia_db': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join("../", 'zajecia.db'),
+        'NAME': BASE_DIR / 'zajecia.db',
     }
 }
 
